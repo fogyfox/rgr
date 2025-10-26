09375e2b5fbbb1143137f86d2bd3607a5edaff67
kitsuneb1 <119156212+fogyfox@users.noreply.github.com>
2025-10-26 18:19:45 +0700

    Delete gronsfeld.h

--- gronsfeld.h
+++
@@ -1,9 +0,0 @@
-#ifndef CIPHER_GRONSFELD_H
-#define CIPHER_GRONSFELD_H
-
-#include <string>
-
-std::string encryptGronsfeld(const std::string& plaintext, const std::string& keyStr, bool useCyrillic = true);
-std::string decryptGronsfeld(const std::string& ciphertext, const std::string& keyStr, bool useCyrillic = true);
-
-#endif
